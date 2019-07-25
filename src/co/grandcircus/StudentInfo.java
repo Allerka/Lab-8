9b99b1526a96af7a01f8855910b5d97b4c72f81b
Brian-Hudson <allerka@gmail.com>
2019-07-25 08:38:03 -0400

    tweaked menu options based on feedback

--- src/co/grandcircus/StudentInfo.java
+++ src/co/grandcircus/StudentInfo.java
@@ -10,21 +10,27 @@ public class StudentInfo {
 		// initial setup
 		Scanner scan = new Scanner(System.in);
 		String cont = "y";
+		String subChoice = "y";
 
 		// user input and displaying returned information
 		System.out.println("Welcome to the student database.");
 		while (cont.equalsIgnoreCase("y")) {
+			subChoice = "y";
 			int selection = Validator.getInt(scan, "Which student would you like to learn more about? (Enter 1-10): ",
 					1, 10);
 			System.out.println("Student " + selection + " is " + names(selection));
-			String information = Validator.getStringMatchingRegex(scan,
-					"Would you like to know their primary language or hometown? ", "(language|hometown)");
-			if (information.equals("language")) {
-				System.out.println(names(selection) + "'s primary language is " + language(selection));
-			} else if (information.equals("hometown")) {
-				System.out.println(names(selection) + " is from " + hometown(selection));
+			while (subChoice.equalsIgnoreCase("y")) {
+				String information = Validator.getStringMatchingRegex(scan,
+						"Would you like to know their: primary language or hometown? ", "(language|hometown)");
+				if (information.equals("language")) {
+					System.out.println(names(selection) + "'s primary language is " + language(selection));
+				} else if (information.equals("hometown")) {
+					System.out.println(names(selection) + " is from " + hometown(selection));
+				}
+				subChoice = Validator.getStringMatchingRegex(scan, "Would you like to know more about them? (y/n) ",
+						"[YyNn]");
 			}
-			cont = Validator.getStringMatchingRegex(scan, "Would you like to know more? (y/n) ", "[YyNn]");
+			cont = Validator.getStringMatchingRegex(scan, "Would you like to see another student? (y/n) ", "[YyNn]");
 		}
 		System.out.println("Exiting!");
 	}
